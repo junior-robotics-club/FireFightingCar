a8bb56e43a5df6ed8dd63fd2ac25c34e9cb02502
neamatullah-JRC <73517544+neamatullah-JRC@users.noreply.github.com>
2023-01-24 23:29:36 +0600

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,43 +1,72 @@
 #include <SoftwareSerial.h>
-
-SoftwareSerial mySerial(3, 2); 
+SoftwareSerial mySerial(3, 2);
+int sensor = 4;
 void setup()
 {
- 
+
+  pinMode(sensor , INPUT_PULLUP);
   Serial.begin(9600);
   mySerial.begin(9600);
-
-  Serial.println("Initializing..."); 
+  
+  Serial.println("Initializing...");
   delay(1000);
-
 }
 
 void loop()
 {
+  int state = digitalRead(sensor);
+  if(state == 0)
+  {
+      sendsms();
+     
+    
+  }
+
+
 }
+
+
 
 void updateSerial()
 {
   delay(500);
-  while (Serial.available()) 
+  while (Serial.available())
   {
     mySerial.write(Serial.read());
   }
-  while(mySerial.available()) 
+  while (mySerial.available())
   {
     Serial.write(mySerial.read());
   }
 }
 
+
+
 void makecall()
 {
-
-
-  mySerial.println("AT"); 
+  mySerial.println("AT");
   updateSerial();
-  mySerial.println("ATD+ +ZZxxxxxxxxxx;");
+  mySerial.println("ATD+ +8801975021104;");
   updateSerial();
   delay(20000);
-  mySerial.println("ATH"); //hang up
+  mySerial.println("ATH");
   updateSerial();
+}
+
+void sendsms()
+{
+
+  mySerial.println("AT"); //Once the handshake test is successful, it will back to OK
+  updateSerial();
+
+  mySerial.println("AT+CMGF=1"); // Configuring TEXT mode
+  updateSerial();
+  mySerial.println("AT+CMGS=\"+8801975021104\"");//change ZZ with country code and xxxxxxxxxxx with phone number to sms
+  updateSerial();
+  mySerial.print("detected"); //text content
+  updateSerial();
+  mySerial.write(26);
+  delay(500);
+  makecall();
+
 }
