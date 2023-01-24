539ad89f0f83949b61ef06b1c766d6fcf8f44f7f
neamatullah-JRC <73517544+neamatullah-JRC@users.noreply.github.com>
2023-01-24 23:12:08 +0600

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,1 +1,43 @@
+#include <SoftwareSerial.h>
 
+SoftwareSerial mySerial(3, 2); 
+void setup()
+{
+ 
+  Serial.begin(9600);
+  mySerial.begin(9600);
+
+  Serial.println("Initializing..."); 
+  delay(1000);
+
+}
+
+void loop()
+{
+}
+
+void updateSerial()
+{
+  delay(500);
+  while (Serial.available()) 
+  {
+    mySerial.write(Serial.read());
+  }
+  while(mySerial.available()) 
+  {
+    Serial.write(mySerial.read());
+  }
+}
+
+void makecall()
+{
+
+
+  mySerial.println("AT"); 
+  updateSerial();
+  mySerial.println("ATD+ +ZZxxxxxxxxxx;");
+  updateSerial();
+  delay(20000);
+  mySerial.println("ATH"); //hang up
+  updateSerial();
+}
