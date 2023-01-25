c01e44cf546b9b69dd43b2620e29b0f888541299
neamatullah-JRC <73517544+neamatullah-JRC@users.noreply.github.com>
2023-01-25 23:08:11 +0600

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,75 +1,72 @@
 #include <SoftwareSerial.h>
-SoftwareSerial mySerial(3, 2);
-int sensor = 4;
-void setup()
-{
+#include <AltSoftSerial.h>
+#include <TinyGPS++.h> 
 
-  pinMode(sensor , INPUT_PULLUP);
-  Serial.begin(9600);
-  mySerial.begin(9600);
+const String PHONE = "+8801975021104";
+const int buttonPin = 10;
+SoftwareSerial sim800(3,2);
+AltSoftSerial neogps;
+TinyGPSPlus gps;
+
+void setup() {
+
+  Serial.begin(115200);
+  sim800.begin(9600);
+  neogps.begin(9600);
   
-  Serial.println("Initializing...");
+  sim800.print("AT+CMGF=1"); 
   delay(1000);
+
 }
 
-void loop()
+void loop() {
+
+
+}
+void sendSmsGPS(String text)
 {
-  int state = digitalRead(sensor);
-  if(state == 0)
+  boolean newData = false;
+  for (unsigned long start = millis(); millis() - start < 2000;)
   {
-     
-   
-      makecall();
-     
-    
+    while (neogps.available())
+    {
+      if (gps.encode(neogps.read()))
+      {
+        newData = true;
+      }
+    }
   }
-
-
-}
-
-
-
-void updateSerial()
-{
-  delay(500);
-  while (Serial.available())
+  if (newData)     
   {
-    mySerial.write(Serial.read());
-  }
-  while (mySerial.available())
-  {
-    Serial.write(mySerial.read());
+    float flat, flon;
+    unsigned long age;
+    Serial.print("Latitude= "); 
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(" Longitude= "); 
+    Serial.println(gps.location.lng(), 6);
+    newData = false;
+    delay(300);
+    sim800.print("AT+CMGF=1\r");
+    delay(1000);
+    sim800.print("AT+CMGS=\""+PHONE+"\"\r");
+    delay(1000);
+    sim800.print("There is too much fire, I can't handle this. Help me ");
+    sim800.print("http://maps.google.com/maps?q=loc:");
+    sim800.print(gps.location.lat(), 6);
+    sim800.print(",");
+    sim800.print(gps.location.lng(), 6);
+    delay(100);
+    sim800.write(0x1A);
+
   }
 }
 
-
-
-void makecall()
-{
-  Serial.println("calling..");
-  mySerial.println("AT");
-  updateSerial();
-  mySerial.println("ATD+ +8801975021104;");
-  updateSerial();
-  delay(20000);
-  mySerial.println("ATH");
-  updateSerial();
-  sendsms();
-}
-
-void sendsms()
-{
-
-  mySerial.println("AT"); //Once the handshake test is successful, it will back to OK
-  updateSerial();
-
-  mySerial.println("AT+CMGF=1"); // Configuring TEXT mode
-  updateSerial();
-  mySerial.println("AT+CMGS=\"+8801975021104\"");//change ZZ with country code and xxxxxxxxxxx with phone number to sms
-  updateSerial();
-  mySerial.print("detected"); //text content
-  updateSerial();
-  mySerial.write(26);
-  delay(500);
-
+void makeCall() {
+  Serial.begin(9600);
+  sim800.println("ATD+8801975021104;"); 
+  delay(15000);
+  sim800.println("ATH");
+  delay(1000);
+  sendSmsGPS("Location");
+  delay(1000);
 }
